4a0948fb661a673cd020b6c6fcd3930017612ca6
Travis Luong <travis@travisluong.com>
2025-01-19 15:37:23 -0800

    add a reset flag to the postgres test script

--- drizzle-next/scripts/test-postgresql.sh
+++ drizzle-next/scripts/test-postgresql.sh
@@ -1,16 +1,37 @@
 DRIZZLE_NEXT_PATH="$HOME/code/drizzle-next/drizzle-next/index.ts"
+RESET=false
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --reset) 
+            RESET=true 
+            shift ;;  # Shift removes the current argument
+        *) 
+            echo "Unknown option: $1" 
+            exit 1 ;;  # Exit for unknown options
+    esac
+done
 
 drizzle_next() {
     tsx "$DRIZZLE_NEXT_PATH" "$@"
 }
 alias drizzle-next="drizzle_next"
 
+if [ "$RESET" = true ]; then
 PGPASSWORD=postgres dropdb -p 5432 -U postgres demo
 PGPASSWORD=postgres createdb -p 5432 -U postgres demo
 rm -rf ~/code/demo-postgresql
+fi
+
 cd ~/code
+
+if [ "$RESET" = true ]; then
 pnpm create next-app@latest demo-postgresql --typescript --eslint --tailwind --app --no-src-dir --no-import-alias --turbopack
+fi
+
 cd demo-postgresql
+
+if [ "$RESET" = true ]; then
 drizzle-next init -p pnpm --latest \
     --db-dialect postgresql \
     -pk cuid2 \
@@ -21,17 +42,33 @@ drizzle-next init -p pnpm --latest \
     --pluralize
 cp ~/code/drizzle-next-env/.env.postgresql .env
 drizzle-next add tiptap
+else
+drizzle-next init -p pnpm --latest \
+    --db-dialect postgresql \
+    -pk cuid2 \
+    -css tailwind \
+    --auth-solution authjs \
+    --auth-providers github,google,postmark,nodemailer,credentials \
+    --admin \
+    --pluralize \
+    --no-install
+fi
+
+drizzle-next add stripe
 drizzle-next scaffold -a admin admin_scaffold -c integer_type:integer smallint_type:smallint bigint_type:bigint serial_type:serial bigserial_type:bigserial boolean_type:boolean text_type:text varchar_type:varchar char_type:char numeric_type:numeric decimal_type:decimal real_type:real double_precision_type:doublePrecision json_type:json jsonb_type:jsonb time_type:time timestamp_type:timestamp: date_type:date file_type:file
 drizzle-next scaffold -a private private_scaffold -c text_field:text integer_field:integer real_field:real decimal_field:decimal boolean_field:boolean file_field:file timestamp_field:timestamp
 drizzle-next scaffold -a public public_scaffold -c text_field:text integer_field:integer real_field:real decimal_field:decimal boolean_field:boolean file_field:file timestamp_field:timestamp
 drizzle-next scaffold -a admin category -c name:text
 drizzle-next scaffold -a admin post -c category_id:references_select title:text likes:integer published_at:timestamp content:text_tiptap
-drizzle-next add stripe
+
+if [ "$RESET" = true ]; then
 npx drizzle-kit generate
 npx drizzle-kit migrate
 npx tsx scripts/create-user.ts user@example.com pw
 npx tsx scripts/create-user.ts admin@example.com pw
 npx tsx scripts/grant-admin.ts admin@example.com
 npx tsx scripts/create-dynamic-product.ts
+fi
+
 npm run build
 npm run dev
