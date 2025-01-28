4248cf4613c61651bc82e6ad083d8afca5a99137
Travis Luong <travis@travisluong.com>
2025-01-28 01:55:42 -0800

    add data types to scaffold help text

--- drizzle-next/commands/scaffold-command.ts
+++ drizzle-next/commands/scaffold-command.ts
@@ -17,6 +17,14 @@ examples:
 npx drizzle-next@latest scaffold category -c name:text
 npx drizzle-next@latest scaffold post -c category_id:references title:text content:text is_published:boolean
 
+postgresql data types:
+integer, smallint, bigint, serial, smallserial, bigserial, boolean, text, varchar, char, numeric, decimal, real, doublePrecision, json, jsonb, time, timestamp, date, uuid
+
+mysql data types:
+int, tinyint, smallint, mediumint, bigint, real, decimal, double, float, serial, binary, varbinary, char, varchar, text, boolean, date, datetime, time, year, timestamp, json
+
+sqlite data types:
+integer, real, text, boolean, bigint, timestamp
 `
   )
   .argument("<table>", "the database table name")
