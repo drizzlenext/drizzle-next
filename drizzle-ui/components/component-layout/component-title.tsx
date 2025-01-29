e4518ade13869b3b18b0a926ce2e6569a978234e
Travis Luong <travis@travisluong.com>
2025-01-29 01:23:22 -0800

    introduction

--- drizzle-ui/components/component-layout/component-title.tsx
+++ drizzle-ui/components/component-layout/component-title.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from "react";
 
 export function ComponentTitle({ children }: { children: ReactNode }) {
   return (
-    <h1 className="font-bold text-4xl border-b border-muted-300">{children}</h1>
+    <h1 className="border-b border-muted-300 text-4xl font-bold dark:border-muted-700">
+      {children}
+    </h1>
   );
 }
