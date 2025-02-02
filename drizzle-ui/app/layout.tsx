c323d0429340de3a657732a9200fd5617086fe68
Travis Luong <travis@travisluong.com>
2025-02-02 14:53:08 -0800

    add gtag to drizzle ui

--- drizzle-ui/app/layout.tsx
+++ drizzle-ui/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "highlight.js/styles/github-dark.css";
 import { DarkModeScript } from "@/components/ui/dark-mode";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-FR34HJSEG9" />
       <DarkModeScript />
     </html>
   );
