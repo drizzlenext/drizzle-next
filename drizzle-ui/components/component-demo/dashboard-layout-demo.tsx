308eb2af159aa6873cffee210888ba825fd9d82c
Travis Luong <travis@travisluong.com>
2025-02-06 01:43:43 -0800

    make the dashboard layout components react forward ref

--- drizzle-ui/components/component-demo/dashboard-layout-demo.tsx
+++ drizzle-ui/components/component-demo/dashboard-layout-demo.tsx
@@ -1,15 +1,53 @@
+"use client";
+
 import {
   DashboardContent,
   DashboardHeader,
   DashboardLayout,
   DashboardSidebar,
+  DashboardSidebarItem,
 } from "@/components/ui/dashboard-layout";
+import { ArrowUpRightIcon, Table2Icon } from "lucide-react";
+import Link from "next/link";
+import { DarkModeToggle } from "@/components/ui/dark-mode";
+import { usePathname } from "next/navigation";
+
+const items = [
+  { text: "Introduction", link: "/introduction", icon: Table2Icon },
+  { text: "Installation", link: "/installation", icon: Table2Icon },
+  { text: "Alert", link: "/components/alert", icon: Table2Icon },
+];
 
 export function DashboardLayoutDemo() {
+  const pathname = usePathname();
+
   return (
     <DashboardLayout>
-      <DashboardHeader />
-      <DashboardSidebar />
+      <DashboardHeader>
+        <div className="px-2 font-mono font-bold">
+          <Link href="/">drizzle-ui</Link>
+        </div>
+        <div className="flex gap-5 px-2">
+          <Link href="/introduction">Docs</Link>
+          <Link
+            href="https://www.drizzle-next.com"
+            className="flex items-center gap-1"
+            target="_blank"
+          >
+            Drizzle Next <ArrowUpRightIcon className="h-4 w-4 text-muted-500" />
+          </Link>
+          <DarkModeToggle />
+        </div>
+      </DashboardHeader>
+      <DashboardSidebar>
+        {items.map((item) => (
+          <Link key={item.link} href={item.link}>
+            <DashboardSidebarItem active={pathname === item.link}>
+              <item.icon className="h-4 w-4" /> {item.text}
+            </DashboardSidebarItem>
+          </Link>
+        ))}
+      </DashboardSidebar>
       <DashboardContent>
         {Array.from({ length: 20 }).map((_, index) => (
           <div key={index} className="p-5">
--- drizzle-ui/components/ui/dashboard-layout.tsx
+++ drizzle-ui/components/ui/dashboard-layout.tsx
@@ -1,72 +1,85 @@
 "use client";
 
-import { ReactNode } from "react";
-import Link from "next/link";
-import { DarkModeToggle } from "./dark-mode";
-import { ArrowUpRightIcon } from "lucide-react";
-import { usePathname } from "next/navigation";
+import * as React from "react";
 import { cn } from "./utils";
 
-export function DashboardLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="grid h-screen grid-cols-[36px_1fr] grid-rows-[auto_1fr] sm:grid-cols-[180px_1fr]">
-      {children}
-    </div>
-  );
-}
+const DashboardLayout = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "grid h-screen grid-cols-[36px_1fr] grid-rows-[auto_1fr] sm:grid-cols-[180px_1fr]",
+      className,
+    )}
+    {...props}
+  />
+));
+DashboardLayout.displayName = "DashboardLayout";
 
-export function DashboardHeader() {
-  return (
-    <div className="col-span-2 row-span-1 gap-2 border-b border-muted-300 bg-gradient-to-t from-primary-100 to-primary-50 dark:border-muted-700 dark:bg-primary-900 dark:from-primary-950 dark:to-primary-900">
-      <div className="flex h-12 w-full items-center justify-between">
-        <div className="px-2 font-mono font-bold">
-          <Link href="/">drizzle-ui</Link>
-        </div>
-        <div className="flex gap-5 px-2">
-          <Link href="/introduction">Docs</Link>
-          <Link
-            href="https://www.drizzle-next.com"
-            className="flex items-center gap-1"
-            target="_blank"
-          >
-            Drizzle Next <ArrowUpRightIcon className="h-4 w-4 text-muted-500" />
-          </Link>
-          <DarkModeToggle />
-        </div>
-      </div>
-    </div>
-  );
-}
+const DashboardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "col-span-2 row-span-1 flex h-12 w-full items-center justify-between gap-2 border-b border-muted-300 bg-gradient-to-t from-primary-100 to-primary-50 dark:border-muted-700 dark:bg-primary-900 dark:from-primary-950 dark:to-primary-900",
+      className,
+    )}
+    {...props}
+  />
+));
+DashboardHeader.displayName = "DashboardHeader";
 
-export function DashboardSidebar() {
-  const items = [
-    { title: "Introduction", url: "/introduction" },
-    { title: "Installation", url: "/installation" },
-    { title: "Alert", url: "/components/alert" },
-  ];
+const DashboardSidebar = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "z-20 col-span-1 row-span-1 flex-col overflow-auto border-r border-muted-300 bg-primary-50 text-sm dark:border-muted-700 dark:bg-primary-950",
+      className,
+    )}
+    {...props}
+  />
+));
+DashboardSidebar.displayName = "DashboardSidebar";
 
-  const pathname = usePathname();
+const DashboardSidebarItem = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & { active: boolean }
+>(({ active, className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "flex flex-row items-center gap-2 overflow-hidden text-nowrap p-2 font-normal hover:bg-primary-100 dark:hover:bg-primary-900",
+      active && "bg-primary-100 dark:bg-primary-900",
+      className,
+    )}
+    {...props}
+  />
+));
+DashboardSidebarItem.displayName = "DashboardSidebarItem";
 
-  return (
-    <div className="z-20 col-span-1 row-span-1 flex-col overflow-auto border-r border-muted-300 bg-primary-50 text-sm dark:border-muted-700 dark:bg-primary-950">
-      {items.map((item) => (
-        <Link
-          key={item.title}
-          href={item.url}
-          className={cn(
-            "flex items-center gap-2 p-2 hover:bg-primary-100 dark:hover:bg-primary-900",
-            pathname === item.url && "bg-primary-100 dark:bg-primary-900",
-          )}
-        >
-          <span className="overflow-hidden text-nowrap font-normal sm:block">
-            {item.title}
-          </span>
-        </Link>
-      ))}
-    </div>
-  );
-}
+const DashboardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("col-span-1 row-span-1 overflow-auto", className)}
+    {...props}
+  />
+));
+DashboardContent.displayName = "DashboardContent";
 
-export function DashboardContent({ children }: { children: ReactNode }) {
-  return <div className="col-span-1 row-span-1 overflow-auto">{children}</div>;
-}
+export {
+  DashboardLayout,
+  DashboardHeader,
+  DashboardSidebar,
+  DashboardSidebarItem,
+  DashboardContent,
+};
