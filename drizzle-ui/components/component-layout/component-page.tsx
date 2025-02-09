9980881cd5c3bd492d6d70310e7e6e2fd55f51f6
Travis Luong <travis@travisluong.com>
2025-02-08 16:49:23 -0800

    component page max w update

--- drizzle-ui/components/component-layout/component-page.tsx
+++ drizzle-ui/components/component-layout/component-page.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function ComponentPage({ children }: { children: ReactNode }) {
   return (
-    <div className="prose dark:prose-invert prose-pre:bg-[#0d1117] m-auto">
+    <div className="prose m-auto !max-w-3xl p-5 dark:prose-invert prose-pre:bg-[#0d1117]">
       {children}
     </div>
   );
