2ca963856004434bb7b0f37dc530a965abfcc3e9
Travis Luong <travis@travisluong.com>
2025-02-22 23:01:26 -0800

    table cell input for various data types

--- drizzle-cms/package/components/drizzle-table.tsx
+++ drizzle-cms/package/components/drizzle-table.tsx
@@ -11,11 +11,18 @@ import {
   TableHead,
   TableRowActions,
   cn,
+  Input,
 } from "drizzle-ui";
 import Link from "next/link";
-import { DrizzleTableConfig } from "../types";
+import { DrizzleTableConfig, SimplifiedColumn } from "../types";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+interface CurrentCell {
+  row: Record<string, any>;
+  col: SimplifiedColumn;
+  el: HTMLTableCellElement;
+}
 
 export function DrizzleTable({
   list,
@@ -28,6 +35,20 @@ export function DrizzleTable({
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const [curRow, setCurRow] = useState<Record<string, any>>(config.curRow);
+  const [curCell, setCurCell] = useState<CurrentCell>();
+  const [curList, setCurList] = useState<Record<string, any>[]>(list);
+
+  useEffect(() => {
+    setCurList(list);
+  }, [list]);
+
+  useEffect(() => {
+    const inputElement = curCell?.el.querySelector("input");
+    if (inputElement) {
+      inputElement.focus();
+      inputElement.select();
+    }
+  }, [curCell]);
 
   function handleClick(row: any) {
     setCurRow(row);
@@ -36,6 +57,63 @@ export function DrizzleTable({
     router.push(`${pathname}?${params.toString()}`);
     const event = new CustomEvent("rowClick", { detail: row });
     window.dispatchEvent(event);
+  }
+
+  function handleDoubleClickCell(
+    e: React.MouseEvent<HTMLTableCellElement>,
+    row: any,
+    col: SimplifiedColumn
+  ) {
+    setCurCell({ row, col, el: e.currentTarget });
+  }
+
+  async function handleBlur(e: React.FocusEvent<HTMLInputElement>) {
+    if (!curCell) {
+      return;
+    }
+    const value = e.target.value;
+    const data: Record<string, any> = {};
+    // special handling for booleans
+    if (curCell.col.dataType === "boolean") {
+      if (value === "f") {
+        data[curCell.col.name] = false;
+      } else if (value === "t") {
+        data[curCell.col.name] = true;
+      } else {
+        data[curCell.col.name] = null;
+      }
+    } else {
+      data[curCell.col.name] = value;
+    }
+
+    const res = await fetch(`/api/${config.curTable}/${curCell.row.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (res.ok) {
+      console.log("patch success");
+      // find and update row in curList
+      const idx = curList.findIndex((record) => record.id === curCell.row.id);
+
+      // if the data type is date, we will need to transform the string back to a date
+      if (curCell.col.dataType === "date") {
+        data[curCell.col.name] = new Date(data[curCell.col.name]);
+      }
+
+      setCurList([
+        ...curList.slice(0, idx),
+        Object.assign({}, curList[idx], data),
+        ...curList.slice(idx + 1),
+      ]);
+    } else {
+      console.error("patch failed");
+      alert("Patch failed.");
+    }
+
+    setCurCell(undefined);
   }
 
   return (
@@ -56,7 +134,7 @@ export function DrizzleTable({
         </TableRow>
       </TableHeader>
       <TableBody>
-        {list.map((row: Record<string, any>) => {
+        {curList.map((row: Record<string, any>) => {
           return (
             <TableRow
               key={row.id}
@@ -70,12 +148,28 @@ export function DrizzleTable({
                 return (
                   <TableCell
                     key={col.name}
-                    className="text-nowrap overflow-clip max-w-28 border"
+                    className={cn(
+                      "text-nowrap overflow-clip max-w-28 border select-none",
+                      isCurrentCell(row, col, curCell) && "p-0"
+                    )}
+                    onDoubleClick={(e) => handleDoubleClickCell(e, row, col)}
                   >
-                    {col.dataType === "date" && row[col.name]?.toLocaleString()}
-                    {col.dataType === "json" && JSON.stringify(row[col.name])}
-                    {col.dataType === "string" && row[col.name]}
-                    {col.dataType === "number" && row[col.name]}
+                    {isCurrentCell(row, col, curCell) ? (
+                      <TableCellInput row={row} col={col} onBlur={handleBlur} />
+                    ) : (
+                      <>
+                        {col.dataType === "date" &&
+                          row[col.name]?.toLocaleString()}
+                        {col.dataType === "json" &&
+                          JSON.stringify(row[col.name])}
+                        {col.dataType === "string" && row[col.name]}
+                        {col.dataType === "number" && row[col.name]}
+                        {col.dataType === "boolean" &&
+                          row[col.name] !== undefined &&
+                          row[col.name] !== null &&
+                          row[col.name].toString()}
+                      </>
+                    )}
                   </TableCell>
                 );
               })}
@@ -103,5 +197,49 @@ export function DrizzleTable({
         })}
       </TableBody>
     </Table>
+  );
+}
+
+function isCurrentCell(
+  row: Record<string, any>,
+  col: SimplifiedColumn,
+  curCell?: CurrentCell
+) {
+  return curCell?.row.id === row.id && col.name === curCell?.col.name;
+}
+
+function TableCellInput({
+  row,
+  col,
+  onBlur,
+}: {
+  row: Record<string, any>;
+  col: SimplifiedColumn;
+  onBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
+}) {
+  if (col.dataType === "boolean") {
+    return (
+      <Input
+        className="border h-10"
+        defaultValue={row[col.name] ? "t" : "f"}
+        onBlur={onBlur}
+      />
+    );
+  } else if (col.dataType === "date") {
+    return (
+      <Input
+        className="border h-10"
+        defaultValue={row[col.name].toLocaleString()}
+        onBlur={onBlur}
+      />
+    );
+  }
+
+  return (
+    <Input
+      className="border h-10"
+      defaultValue={row[col.name]}
+      onBlur={onBlur}
+    />
   );
 }
