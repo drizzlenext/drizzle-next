299ace781a43a94a75251dcb2f8ada8f600edcc8
Travis Luong <travis@travisluong.com>
2025-02-27 23:58:23 -0800

    create a minimal typography css file

--- drizzle-ui/app/(docs)/(pages)/layout.tsx
+++ drizzle-ui/app/(docs)/(pages)/layout.tsx
@@ -1,3 +1,5 @@
+import "@/styles/typography.css";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="m-auto !max-w-3xl p-3">{children}</div>;
+  return <div className="typography m-auto !max-w-3xl p-3">{children}</div>;
 }
