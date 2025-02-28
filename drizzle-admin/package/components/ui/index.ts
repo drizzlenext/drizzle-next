18ec549261f4c5d7e1546984c4963f2f2fd65b99
Travis Luong <travis@travisluong.com>
2025-02-28 02:44:25 -0800

    update build css with new tailwind cli

---
+++ drizzle-admin/package/components/ui/index.ts
@@ -0,0 +1,50 @@
+export { Alert } from "./alert";
+export { Avatar } from "./avatar";
+export { Button } from "./button";
+export { Card } from "./card";
+export { Checkbox } from "./checkbox";
+export { cn } from "./utils";
+export { CodeBlock } from "./code-block";
+export { DarkModeScript, DarkModeToggle } from "./dark-mode";
+export {
+  DashboardLayout,
+  DashboardSidebarToggle,
+  DashboardHeader,
+  DashboardTitle,
+  DashboardNav,
+  DashboardNavToggle,
+  DashboardSidebar,
+  DashboardSidebarGroup,
+  DashboardSidebarLabel,
+  DashboardSidebarItem,
+  DashboardContent,
+} from "./dashboard-layout";
+export { Form, FormControl, FormMessage } from "./form";
+export { Input } from "./input";
+export { Label } from "./label";
+export {
+  PageLayout,
+  PageContent,
+  PageFooter,
+  PageHeader,
+  PageNav,
+  PageTitle,
+  PageAside,
+  PageAsideToggle,
+} from "./page-layout";
+export { Pagination } from "./pagination";
+export { SearchInput } from "./search-input";
+export { Select, SelectOption } from "./select";
+export { Sortable } from "./sortable";
+export {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableRowActions,
+} from "./table";
+export { Textarea } from "./textarea";
