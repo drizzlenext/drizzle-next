66f061e8de4de70dcdc427f5919efbfa31a5f7d9
Travis Luong <travis@travisluong.com>
2025-03-24 22:19:21 -0700

    style the scaffolds

--- example/app/(admin)/_lib/drizzle-admin.config.ts
+++ example/app/(admin)/_lib/drizzle-admin.config.ts
@@ -19,6 +19,7 @@ export const config: DrizzleAdminConfig = {
     },
     categories: {
       drizzleTable: categories,
+      searchBy: ["id", "name"]
     },
     tags: {
       drizzleTable: tags,
