60b49f9b2e6706288de726acf251e0493d44947a
Travis Luong <travis@travisluong.com>
2026-03-29 23:24:14 -0700

    change base path if github page

--- drizzle-ui/next.config.ts
+++ drizzle-ui/next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+const githubPagesBasePath = "/drizzle-next/ui";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: ""
+  assetPrefix: isGitHubActions ? githubPagesBasePath : "",
+  basePath: isGitHubActions ? githubPagesBasePath : ""
 };
 
 export default nextConfig;
