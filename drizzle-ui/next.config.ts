5f6fc018af14402c900cc6f28397c3e7a1e2724d
Travis Luong <travis@travisluong.com>
2026-03-29 23:16:24 -0700

    change base path

--- drizzle-ui/next.config.ts
+++ drizzle-ui/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/ui"
+  basePath: ""
 };
 
 export default nextConfig;
